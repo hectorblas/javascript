8e212ca60feb583334fede0c1bcdef56795e1431
hectorblas <35321826+hectorblas@users.noreply.github.com>
2018-03-15 18:18:41 -0500

    Update metodos.js

--- js/metodos.js
+++ js/metodos.js
@@ -1,6 +1,7 @@
 // variables
-var valor1;
+var valor1=0;
 var valor2;
+
 var operacion;
 //eventos para los numeros
 document.getElementById("0").addEventListener('click', cero, false);
@@ -26,9 +27,166 @@ document.getElementById("por").addEventListener('click',multiplicacion, false);
 document.getElementById("dividido").addEventListener('click',division, false);
 //resultado
 document.getElementById("igual").addEventListener('click',resultado, false);
+//funcion para el evento mouse
+document.getElementById('0').onmousedown=cero1;
+document.getElementById('0').onmouseup=cero2;
+document.getElementById('1').onmousedown=uno1;
+document.getElementById('1').onmouseup=uno2;
+document.getElementById('2').onmousedown=dos1;
+document.getElementById('2').onmouseup=dos2;
+document.getElementById('3').onmousedown=tres1;
+document.getElementById('3').onmouseup=tres2;
+document.getElementById('4').onmousedown=cuatro1;
+document.getElementById('4').onmouseup=cuatro2;
+document.getElementById('5').onmousedown=cinco1;
+document.getElementById('5').onmouseup=cinco2;
+document.getElementById('6').onmousedown=seis1;
+document.getElementById('6').onmouseup=seis2;
+document.getElementById('7').onmousedown=siete1;
+document.getElementById('7').onmouseup=siete2;
+document.getElementById('8').onmousedown=ocho1;
+document.getElementById('8').onmouseup=ocho2;
+document.getElementById('9').onmousedown=nueve1;
+document.getElementById('9').onmouseup=nueve2;
+document.getElementById('on').onmousedown=on1;
+document.getElementById('on').onmouseup=on2;
+document.getElementById('punto').onmousedown=punto1;
+document.getElementById('punto').onmouseup=punto2;
+document.getElementById('sign').onmousedown=sign1;
+document.getElementById('sign').onmouseup=sign2;
+document.getElementById('mas').onmousedown=mas1;
+document.getElementById('mas').onmouseup=mas2;
+document.getElementById('menos').onmousedown=menos1;
+document.getElementById('menos').onmouseup=menos2;
+document.getElementById('por').onmousedown=por1;
+document.getElementById('por').onmouseup=por2;
+document.getElementById('dividido').onmousedown=dividido1;
+document.getElementById('dividido').onmouseup=dividido2;
+document.getElementById('raiz').onmousedown=raiz1;
+document.getElementById('raiz').onmouseup=raiz2;
+document.getElementById('igual').onmousedown=igual1;
+document.getElementById('igual').onmouseup=igual2;
+
+//funcion para tamaño de los bototones
+
+function cero1() {
+  document.getElementById("0").style="filter:drop-shadow(0 0 7px #26C5F0);transform:scale(0.95)";
+}
+function cero2() {
+  document.getElementById("0").style="none";
+}
+function uno1() {
+  document.getElementById("1").style="filter:drop-shadow(0 0 7px #26C5F0);transform:scale(0.95)";
+}
+function uno2() {
+  document.getElementById("1").style="none";
+}
+function dos1() {
+  document.getElementById("2").style="filter:drop-shadow(0 0 7px #26C5F0);transform:scale(0.95)";
+}
+function dos2() {
+  document.getElementById("2").style="none";
+}
+function tres1() {
+  document.getElementById("3").style="filter:drop-shadow(0 0 7px #26C5F0);transform:scale(0.95)";
+}
+function tres2() {
+  document.getElementById("3").style="none";
+}
+function cuatro1() {
+  document.getElementById("4").style="filter:drop-shadow(0 0 7px #26C5F0);transform:scale(0.95)";
+}
+function cuatro2() {
+  document.getElementById("4").style="none";
+}
+function cinco1() {
+  document.getElementById("5").style="filter:drop-shadow(0 0 7px #26C5F0);transform:scale(0.95)";
+}
+function cinco2() {
+  document.getElementById("5").style="none";
+}
+function seis1() {
+  document.getElementById("6").style="filter:drop-shadow(0 0 7px #26C5F0);transform:scale(0.95)";
+}
+function seis2() {
+  document.getElementById("6").style="none";
+}
+function siete1() {
+  document.getElementById("7").style="filter:drop-shadow(0 0 7px #26C5F0);transform:scale(0.95)";
+}
+function siete2() {
+  document.getElementById("7").style="none";
+}
+function ocho1() {
+  document.getElementById("8").style="filter:drop-shadow(0 0 7px #26C5F0);transform:scale(0.95)";
+}
+function ocho2() {
+  document.getElementById("8").style="none";
+}
+function nueve1() {
+  document.getElementById("9").style="filter:drop-shadow(0 0 7px #26C5F0);transform:scale(0.95)";
+}
+function nueve2() {
+  document.getElementById("9").style="none";
+}
+function on1() {
+  document.getElementById("on").style="filter:drop-shadow(0 0 7px #26C5F0);transform:scale(0.95)";
+}
+function on2() {
+  document.getElementById("on").style="none";
+}
+function punto1() {
+  document.getElementById("punto").style="filter:drop-shadow(0 0 7px #26C5F0);transform:scale(0.95)";
+}
+function punto2() {
+  document.getElementById("punto").style="none";
+}
+function sign1() {
+  document.getElementById("sign").style="filter:drop-shadow(0 0 7px #26C5F0);transform:scale(0.95)";
+}
+function sign2() {
+  document.getElementById("sign").style="none";
+}
+function mas1() {
+  document.getElementById("mas").style="filter:drop-shadow(0 0 7px #26C5F0);transform:scale(0.95)";
+}
+function mas2() {
+  document.getElementById("mas").style="none";
+}
+function menos1() {
+  document.getElementById("menos").style="filter:drop-shadow(0 0 7px #26C5F0);transform:scale(0.95)";
+}
+function menos2() {
+  document.getElementById("menos").style="none";
+}
+function por1() {
+  document.getElementById("por").style="filter:drop-shadow(0 0 7px #26C5F0);transform:scale(0.95)";
+}
+function por2() {
+  document.getElementById("por").style="none";
+}
+function dividido1() {
+  document.getElementById("dividido").style="filter:drop-shadow(0 0 7px #26C5F0);transform:scale(0.95)";
+}
+function dividido2() {
+  document.getElementById("dividido").style="none";
+}
+function igual1() {
+  document.getElementById("igual").style="filter:drop-shadow(0 0 7px #26C5F0);transform:scale(0.95)";
+}
+function igual2() {
+  document.getElementById("igual").style="none";
+}
+function raiz1() {
+  document.getElementById("raiz").style="filter:drop-shadow(0 0 7px #26C5F0);transform:scale(0.95)";
+}
+function raiz2() {
+  document.getElementById("raiz").style="none";
+}
 
 //funcion para poner los numeros
 function cero() {
+
     valor=document.getElementById("display").innerHTML;
     if (valor=="0"){
         document.getElementById("display").innerHTML ="0";
@@ -142,25 +300,44 @@ function signo() {
 }
 //funcion para las operacion matematicas
 function suma() {
-  valor1=document.getElementById("display").innerHTML;
-  operacion="+"
-  document.getElementById("display").innerHTML="";
+  valors=document.getElementById("display").innerHTML;
+  if (valors=="") {
+    valors=0;
+    valor1=valor1+valors;
+  }else
+    valor1=parseFloat(valor1)+parseFloat(valors);
+    operacion="+"
+    document.getElementById("display").innerHTML="";
 }
 
 function resta() {
-  valor1=document.getElementById("display").innerHTML;
+  valors=document.getElementById("display").innerHTML;
+  if (valors=="") {
+    valors=0;
+    valor1=valor1+valors;
+  }else
+  valor1=parseFloat(valor1)+parseFloat(valors);
   operacion="-"
   document.getElementById("display").innerHTML="";
 }
-
 function multiplicacion() {
-  valor1=document.getElementById("display").innerHTML;
+  valors=document.getElementById("display").innerHTML;
+  if (valors=="") {
+    valors=0;
+    valor1=valor1+valors;
+  }else
+  valor1=parseFloat(valor1)+parseFloat(valors);
   operacion="*"
   document.getElementById("display").innerHTML="";
 }
 
 function division() {
-  valor1=document.getElementById("display").innerHTML;
+  valors=document.getElementById("display").innerHTML;
+  if (valors=="") {
+    valors=0;
+    valor1=valor1+valors;
+  }else
+  valor1=parseFloat(valor1)+parseFloat(valors);
   operacion="/"
   document.getElementById("display").innerHTML="";
 }
@@ -173,22 +350,22 @@ function resultado() {
 function resolver() {
   switch (operacion) {
     case "+":
-      var res=parseFloat(valor1)+parseFloat(valor2);
+      var res=valor1+parseFloat(valor2);
       document.getElementById("display").innerHTML=res;
       validar();
       break;
       case "-":
-        var res=parseFloat(valor1)-parseFloat(valor2);
+        var res=valor1-parseFloat(valor2);
         document.getElementById("display").innerHTML=res;
         validar();
         break;
       case "*":
-        var res=parseFloat(valor1)*parseFloat(valor2);
+        var res=valor1*parseFloat(valor2);
         document.getElementById("display").innerHTML=res;
         validar();
         break;
       case "/":
-        var res=parseFloat(valor1)/parseFloat(valor2);
+        var res=valor1/parseFloat(valor2);
         document.getElementById("display").innerHTML=res;
         validar();
       break;
